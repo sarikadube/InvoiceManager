d72c26426d01053ba13f286911e0802d2c1594f5
sarikadube <ramanadham.raj@gmail.com>
2020-08-16 10:50:06 +0100

    Second commit

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,18 +19,19 @@ public class LoginSteps {
     }
 
     @When("^I enter username \"([^\"]*)\"$")
-    public void i_enter_username(String arg1){
+    public void i_enter_username(String username){
+        driver.findElement(By.id("inputEmail")).sendKeys(username);
 
     }
 
     @When("^I  enter password  \"([^\"]*)\"$")
-    public void i_enter_password(String arg1){
-
+    public void i_enter_password(String password){
+        driver.findElement(By.id("inputPassword")).sendKeys(password);
     }
 
     @When("^I clieck login button$")
     public void i_clieck_login_button(){
-
+        driver.findElement(By.xpath("/html/body/div/div/div/div/div[2]/div[2]/div/form/div[3]/div[2]/button")).click();
     }
 
     @Then("^I should login$")
